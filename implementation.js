e0cfc5c3118ae8d5519126540e40995b1e2ea60a
Hack-yao <1018915068@qq.com>
2019-03-24 12:48:49 +0800

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -12,20 +12,20 @@ module.exports = function bind(that) {
     if (typeof target !== 'function' || toStr.call(target) !== funcType) {
         throw new TypeError(ERROR_MESSAGE + target);
     }
-    var args = slice.call(arguments, 1);
+    var args = slice.call(arguments, 1);  // 0 是 that
 
     var bound;
     var binder = function () {
-        if (this instanceof bound) {
+        if (this instanceof bound) { // bound 作为 new 构造函数使用
             var result = target.apply(
                 this,
                 args.concat(slice.call(arguments))
             );
-            if (Object(result) === result) {
+            if (Object(result) === result) { // target函数 返回一个对象，使用target返回值
                 return result;
             }
-            return this;
-        } else {
+            return this;  // 返回 构造函数中的this对象
+        } else { // 正常函数调用
             return target.apply(
                 that,
                 args.concat(slice.call(arguments))
@@ -35,17 +35,20 @@ module.exports = function bind(that) {
 
     var boundLength = Math.max(0, target.length - args.length);
     var boundArgs = [];
-    for (var i = 0; i < boundLength; i++) {
+    for (var i = 0; i < boundLength; i++) { // 剩余缺少的参数， 保持函数的length值
         boundArgs.push('$' + i);
     }
 
     bound = Function('binder', 'return function (' + boundArgs.join(',') + '){ return binder.apply(this,arguments); }')(binder);
+//     bound = function ($1,$2, ...) {
+//         return binder.apply(this, arguments);
+//     }
 
     if (target.prototype) {
         var Empty = function Empty() {};
         Empty.prototype = target.prototype;
-        bound.prototype = new Empty();
-        Empty.prototype = null;
+        bound.prototype = new Empty();  // 这样 bound.prototype上添加的属性不会影响到 target.prototype，实现了一定程度上的隔离
+        Empty.prototype = null;  // 销毁 Empty.prototype对target.prototype的引用。防止一些未释放的引用造成的内存泄漏
     }
 
     return bound;
